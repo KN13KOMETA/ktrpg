0728d74637ea81bf8f313d2cacc07b8834f26462
ClintFlames <41988734+KN13KOMETA@users.noreply.github.com>
2024-12-16 23:34:13 +0300

    Character and Weapon structs

---
+++ src/character.h
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+#include "const.h"
+#include "weapon.h"
+
+struct Character {
+  char name[CHARACTER_NAME_LENGTH];
+  uint32_t gold;
+  uint16_t max_health;
+  uint16_t health;
+  struct Weapon weapon;
+};
+
+void debug_character(struct Character *chr);
+void print_player(struct Character *chr);
+void print_enemy(struct Character *chr);
+
+void fix_character(struct Character *chr);
+struct Character generate_player(char *name);
+struct Character generate_enemy(uint8_t difficulty);
---
+++ src/weapon.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+#include "const.h"
+
+struct Weapon {
+  char name[WEAPON_NAME_LENGTH];
+  uint32_t price;
+  uint32_t upgrade_price;
+  uint16_t damage;
+};
