444c3255ac5abafb90b15fd4a1545fc67a70a56d
ClintFlames <41988734+KN13KOMETA@users.noreply.github.com>
2024-12-17 23:08:52 +0300

    Preparation for location code

--- src/location/location.c
+++ src/location/location.c
@@ -1,8 +1,13 @@
-
-
+#include <stdint.h>
 #include <stdio.h>
+
 void location_loop(void) {
+  uint8_t location_id = 0;
   while (1) {
-    printf("LOCATION LOOP\n");
+    switch (location_id) {
+      case 0: {
+        break;
+      }
+    }
   }
 }
