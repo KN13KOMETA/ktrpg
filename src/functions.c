c973c98163ae6528c676382945b1e7d8dc212550
ClintFlames <41988734+KN13KOMETA@users.noreply.github.com>
2024-12-17 20:04:09 +0300

    Make getchar_clear(), clear stdin completely

--- src/functions.c
+++ src/functions.c
@@ -3,6 +3,11 @@
 // Reads char from stdin and clears it's buffer
 char getchar_clear(void) {
   char c = getchar();
-  if (c != 10) getchar();
+
+  if (c != 10) {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF);
+  }
+
   return c;
 }
