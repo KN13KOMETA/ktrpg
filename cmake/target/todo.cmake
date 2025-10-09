1eeb39bc28066b4ba395af6b17d6ea747f259a39
KN13KOMETA <kn13kometa@gmail.com>
2025-10-09 20:18:25 +0300

    Search for "TODO:" instead of "TODO"

--- cmake/target/todo.cmake
+++ cmake/target/todo.cmake
@@ -12,6 +12,6 @@ set(
 add_custom_target(
   todo
   COMMAND
-    cd ${CMAKE_SOURCE_DIR} && grep -e TODO -Hnr . ${TODO_EXCLUDE} | awk -f
+    cd ${CMAKE_SOURCE_DIR} && grep -e "TODO:" -Hnr . ${TODO_EXCLUDE} | awk -f
     ${CMAKE_SOURCE_DIR}/scripts/awk/todo.awk
 )
