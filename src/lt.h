2273ab9b036374ade5b1e5504bdf93dc280a6112
KN13KOMETA <kn13kometa@gmail.com>
2025-11-14 21:48:39 +0300

    Playing with lua

---
+++ src/lt.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef LT_H
+#define LT_H
+
+void ltest(void);
+
+#endif  // !LT_H
--- src/main.c
+++ src/main.c
@@ -9,18 +9,21 @@
 #define PICO_ECS_IMPLEMENTATION
 #include <pico_ecs.h>
 
+#include "lt.h"
+
 int main(void) {
-  ecs_t *ecs = ecs_new(64, NULL);
+  ltest();
+  ecs_t* ecs = ecs_new(64, NULL);
 
   register_components(ecs);
   register_systems(ecs);
 
   ecs_entity_t player = ecs_create(ecs);
 
-  debug_comp *player_debug = ecs_add(ecs, player, DEBUG_COMP, NULL);
+  debug_comp* player_debug = ecs_add(ecs, player, DEBUG_COMP, NULL);
   *player_debug = 1;
 
-  character_name_comp *player_name =
+  character_name_comp* player_name =
       ecs_add(ecs, player, CHARACTER_NAME_COMP, NULL);
   {
     *player_name = malloc(MAX_NAME_LENGTH);
@@ -29,15 +32,15 @@ int main(void) {
     if (getchars_clear(*player_name, MAX_NAME_LENGTH) == EOF) return 1;
   }
 
-  character_health_comp *player_health =
+  character_health_comp* player_health =
       ecs_add(ecs, player, CHARACTER_HEALTH_COMP, NULL);
   player_health->max = 20;
   player_health->current = 10;
 
-  weapon_name_comp *weapon_name = ecs_add(ecs, player, WEAPON_NAME_COMP, NULL);
+  weapon_name_comp* weapon_name = ecs_add(ecs, player, WEAPON_NAME_COMP, NULL);
   *weapon_name = "sex";
 
-  weapon_damage_comp *weapon_damage =
+  weapon_damage_comp* weapon_damage =
       ecs_add(ecs, player, WEAPON_DAMAGE_COMP, NULL);
   *weapon_damage = 66;
 
