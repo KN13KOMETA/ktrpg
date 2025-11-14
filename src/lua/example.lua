90bea1cf7d09dcc0dad72a171b41e8c167e951ae
KN13KOMETA <kn13kometa@gmail.com>
2025-11-14 21:48:39 +0300

    Playing with lua

---
+++ src/lua/example.lua
@@ -0,0 +1,11 @@
+function init_arr()
+  return { 10, 20, 30, 40, 50 }
+end
+
+function get_weapon_data()
+  return {
+    { name = "Sword", damage = 15, price = 100 },
+    { name = "Axe",   damage = 20, price = 150 },
+    { name = "Bow",   damage = 12, price = 80 }
+  }
+end
