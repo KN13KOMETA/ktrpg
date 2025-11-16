db5274099f54f021a704e9a0fb78ca69a0bf48db
KN13KOMETA <kn13kometa@gmail.com>
2025-11-17 02:58:27 +0300

    Some play with getopt

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
+#include <getopt.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "functions.h"
 #include "game/ecs/components.h"
@@ -13,7 +15,56 @@
 
 #include "lt.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
+  {
+    // Do not print error
+    opterr = 0;
+
+    int opt = '?';
+    while ((opt = getopt(argc, argv, "VH")) != -1) {
+      switch (opt) {
+        case 'V':
+          goto process_option;
+        case 'L':
+          goto process_option;
+        case 'H':
+          goto process_option;
+        default:
+          goto process_option;
+      }
+    }
+
+  process_option:
+    switch (opt) {
+      case 'V':
+        printf("%s %s\n\n", PROJECT_NAME, PROJECT_FULL_VERSION);
+        break;
+      case 'H':
+        printf("%s\n\n", PROJECT_DESCRIPTION);
+
+        printf("Usage: %s [OPTION]\n\n", argv[0]);
+
+        printf(
+            "Options:\n"
+            "  -V, --version               "
+            "prints program version\n"
+            "  -I, --internal              "
+            "load game with internal lua scripts\n"
+            "  -X [PATH], --extract [PATH] "
+            "extracts internal lua scripts to given path\n"
+            "  -L PATH, --lua PATH         "
+            "path to lua script for custom content\n"
+            "  -H, --help                  "
+            "prints this text\n"
+            "\n");
+
+        printf("Project homepage: %s\n", PROJECT_HOMEPAGE_URL);
+        break;
+      default:
+        printf("SWW: Unknown option.");
+    }
+  }
+
   ecs_t* tgg_ecs = tgg_init();
 
   tgg_load_content(tgg_ecs, true);
@@ -25,7 +76,7 @@ int main(void) {
   tgg_free(tgg_ecs);
 
   ltest();
-  return 0;
+
   ecs_t* ecs = ecs_new(64, NULL);
 
   register_components(ecs);
