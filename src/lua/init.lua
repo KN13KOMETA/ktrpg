c3ee1f5dc0d8514fc9965d1c3342483454081313
KN13KOMETA <kn13kometa@gmail.com>
2025-11-16 03:14:22 +0300

    Finally able to register entity and components from lua

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -1,29 +1,7 @@
-function init_arr()
-  return { 10, 20, 30, 40, 50 }
-end
-
-function get_weapon_data()
-  return {
-    { name = "Sword", damage = 15, price = 100 },
-    { name = "Axe",   damage = 20, price = 150 },
-    { name = "Bow",   damage = 12, price = 80 },
-  }
-end
-
--- sex
-
-function init()
-  return 102
-end
-
-local l = bruh(7, 6)
-print("ssss")
-print(l)
--- reg(101)
-reg({ somenum = 69 })
-
-register_enemy({
-  health = 17,
-  num = 99,
+tgg_register_character({
   name = "Sexmaster69",
+})
+
+tgg_register_character({
+  name = "Goblin",
 })
--- src/tgg/tgg.c
+++ src/tgg/tgg.c
@@ -1,7 +1,18 @@
 #include "tgg.h"
 
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../luah/init.h"
 #include "components.h"
 #include "systems.h"
+
+int tgg_register_character(lua_State* L);
 
 ecs_t* tgg_init(void) {
   ecs_t* tgg_ecs = ecs_new(64, NULL);
@@ -12,4 +23,88 @@ ecs_t* tgg_init(void) {
   return tgg_ecs;
 }
 
+int tgg_load_content(ecs_t* tgg_ecs, bool force_internal) {
+  // TODO: Maybe somehow try to make it sandbox safe
+  // NOTE: luaopen_package should be called later
+  lua_State* L = luaL_newstate();
+  // Open some libs
+  luaopen_base(L);
+  luaopen_math(L);
+  luaopen_string(L);
+  luaopen_table(L);
+  // luaopen_io(L);
+  // luaopen_os(L);
+  // luaopen_package(L);
+  // luaopen_debug(L);
+  luaopen_bit(L);
+  // luaopen_jit(L);
+  // luaopen_ffi(L);
+  // luaopen_string_buffer(L);
+
+  {
+    ecs_entity_t player = ecs_create(tgg_ecs);
+
+    tgg_character_name_comp* player_name =
+        ecs_add(tgg_ecs, player, TGG_CHARACTER_NAME_COMP, NULL);
+
+    *player_name = "Not";
+  }
+
+  lua_pushlightuserdata(L, tgg_ecs);
+
+  lua_pushcclosure(L, tgg_register_character, 1);
+  // lua_pushcfunction(L, tgg_register_character);
+  lua_setglobal(L, "tgg_register_character");
+
+  // Load internal scripts in case if forced internal or init.lua does'n exists
+  if (force_internal || access("init.lua", F_OK) != 0) {
+    printf("Loading content using internal scripts\n");
+
+    if (luaL_dostring(L, init_lua) != LUA_OK) {
+      printf("Error at internal scripts: %s\n", lua_tostring(L, -1));
+
+      return 1;
+    }
+  } else {
+    // TODO: user scripts load
+  }
+
+  lua_close(L);
+
+  return 0;
+}
+
 void tgg_free(ecs_t* tggecs) { ecs_free(tggecs); }
+
+// Registers
+int tgg_register_character(lua_State* L) {
+  ecs_t* tgg_ecs = lua_touserdata(L, lua_upvalueindex(1));
+  ecs_entity_t character = ecs_create(tgg_ecs);
+
+  // If arg is not table return an error
+  if (!lua_istable(L, -1)) luaL_typerror(L, -1, "table");
+
+  {
+    lua_getfield(L, -1, "name");
+    const char* field = lua_tostring(L, -1);
+
+    if (field != NULL) {
+      tgg_character_name_comp* name =
+          ecs_add(tgg_ecs, character, TGG_CHARACTER_NAME_COMP, NULL);
+
+      *name = malloc(strlen(field));
+
+      strcpy(*name, field);
+    }
+  }
+
+  {
+    lua_getfield(L, -2, "health");
+    lua_Number health = lua_tonumber(L, -1);
+
+    printf("health: %f\n", health);
+  }
+
+  // We don't return anything
+  return 0;
+}
