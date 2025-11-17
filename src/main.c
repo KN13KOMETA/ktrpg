9956aa2c33a7c342806fc5cfd43cb59f8da18549
KN13KOMETA <kn13kometa@gmail.com>
2025-11-18 01:40:03 +0300

    Replace getopt with argparse

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-#include <getopt.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
+#include "argparse.h"
 #include "functions.h"
 #include "game/ecs/components.h"
 #include "game/ecs/systems.h"
@@ -13,55 +12,76 @@
 #define PICO_ECS_IMPLEMENTATION
 #include <pico_ecs.h>
 
+static const char* const usages[] = {
+    "basic [options] [[--] args]",
+    "basic [options]",
+    NULL,
+};
+
+#define PERM_READ (1 << 0)
+#define PERM_WRITE (1 << 1)
+#define PERM_EXEC (1 << 2)
+
+struct args {
+  bool version;
+};
+
 int main(int argc, char* argv[]) {
   {
-    // Do not print error
-    opterr = 0;
+    int version = 0;
+    int force = 0;
+    int test = 0;
+    int int_num = 0;
+    float flt_num = 0.f;
+    const char* path = NULL;
+    int perms = 0;
+    struct argparse_option options[] = {
 
-    int opt = '?';
-    while ((opt = getopt(argc, argv, "VH")) != -1) {
-      switch (opt) {
-        case 'V':
-          goto process_option;
-        case 'L':
-          goto process_option;
-        case 'H':
-          goto process_option;
-        default:
-          goto process_option;
+        // "prints program version\n"
+        // "  -I, --internal              "
+        // "load game with internal lua scripts\n"
+        // "  -X [PATH], --extract [PATH] "
+        // "extracts internal lua scripts to given path\n"
+        // "  -L PATH, --lua PATH         "
+        // "path to lua script for custom content\n"
+        OPT_HELP(),
+        OPT_GROUP("Basic options"),
+        OPT_BOOLEAN('v', "version", &version, "shows version", NULL, 0, 0),
+        OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
+        OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
+        OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
+        OPT_INTEGER('i', "int", &int_num, "selected integer", NULL, 0, 0),
+        OPT_FLOAT('s', "float", &flt_num, "selected float", NULL, 0, 0),
+        OPT_GROUP("Bits options"),
+        OPT_BIT(0, "read", &perms, "read perm", NULL, PERM_READ, OPT_NONEG),
+        OPT_BIT(0, "write", &perms, "write perm", NULL, PERM_WRITE, 0),
+        OPT_BIT(0, "exec", &perms, "exec perm", NULL, PERM_EXEC, 0),
+        OPT_END(),
+    };
+
+    struct argparse argparse;
+    argparse_init(&argparse, options, usages, 0);
+    argparse_describe(&argparse, "\n" PROJECT_DESCRIPTION,
+                      "\nHomepage: " PROJECT_HOMEPAGE_URL);
+    argc = argparse_parse(&argparse, argc, argv);
+    if (force != 0) printf("force: %d\n", force);
+    if (test != 0) printf("test: %d\n", test);
+    if (path != NULL) printf("path: %s\n", path);
+    if (int_num != 0) printf("int_num: %d\n", int_num);
+    if (flt_num != 0) printf("flt_num: %g\n", flt_num);
+    if (argc != 0) {
+      printf("argc: %d\n", argc);
+      int i;
+      for (i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, *(argv + i));
       }
     }
-
-  process_option:
-    switch (opt) {
-      case 'V':
-        printf("%s %s\n\n", PROJECT_NAME, PROJECT_FULL_VERSION);
-        break;
-      case 'H':
-        printf("%s\n\n", PROJECT_DESCRIPTION);
-
-        printf("Usage: %s [OPTION]\n\n", argv[0]);
-
-        printf(
-            "Options:\n"
-            "  -V, --version               "
-            "prints program version\n"
-            "  -I, --internal              "
-            "load game with internal lua scripts\n"
-            "  -X [PATH], --extract [PATH] "
-            "extracts internal lua scripts to given path\n"
-            "  -L PATH, --lua PATH         "
-            "path to lua script for custom content\n"
-            "  -H, --help                  "
-            "prints this text\n"
-            "\n");
-
-        printf("Project homepage: %s\n", PROJECT_HOMEPAGE_URL);
-        break;
-      default:
-        printf("SWW: Unknown option.");
+    if (perms) {
+      printf("perms: %d\n", perms);
     }
   }
+
+  printf("AMONGUS. START OF GAME.\n\n");
 
   ecs_t* tgg_ecs = tgg_init();
 
