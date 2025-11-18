2ce334e3d12db7a09e22ee13aaf10914b2f6a4d2
KN13KOMETA <kn13kometa@gmail.com>
2025-11-18 20:28:39 +0300

    More arg parse code

--- src/prompt_options.h
+++ src/prompt_options.h
@@ -2,6 +2,7 @@
 #ifndef PROMPT_OPTIONS
 #define PROMPT_OPTIONS
 
-void prompt_options(int argc, const char* argv[]);
+void prompt_options(int argc, const char* argv[], char* script_path,
+                    char* save_path);
 
 #endif  // !PROMPT_OPTIONS
