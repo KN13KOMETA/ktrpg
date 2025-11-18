97ffade9f6764b3fe023addeea01975093dbd8b9
KN13KOMETA <kn13kometa@gmail.com>
2025-11-18 20:44:49 +0300

    Add project_options struct

--- src/prompt_options.h
+++ src/prompt_options.h
@@ -2,7 +2,13 @@
 #ifndef PROMPT_OPTIONS
 #define PROMPT_OPTIONS
 
-void prompt_options(int argc, const char* argv[], char* script_path,
-                    char* save_path);
+typedef struct {
+  char* script_path;
+  char* save_path;
+} project_options;
+
+project_options* prompt_options(int argc, const char* argv[]);
+
+void free_project_options(project_options* options);
 
 #endif  // !PROMPT_OPTIONS
