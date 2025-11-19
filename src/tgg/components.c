d7434a1567e9b445455eb6fff7b0b787e55669e5
KN13KOMETA <kn13kometa@gmail.com>
2025-11-19 20:17:48 +0300

    Move debug function from header file

--- src/tgg/components.c
+++ src/tgg/components.c
@@ -1,5 +1,7 @@
 #include "components.h"
 
+#include <stdio.h>
+
 ecs_comp_t TGG_CHARACTER_NAME_COMP;
 ecs_comp_t TGG_CHARACTER_MIN_HEALTH_COMP;
 ecs_comp_t TGG_CHARACTER_MAX_HEALTH_COMP;
@@ -11,4 +13,14 @@ void tgg_register_components(ecs_t* ecs) {
       ecs, sizeof(tgg_character_min_health_comp), NULL, NULL);
   TGG_CHARACTER_MAX_HEALTH_COMP = ecs_define_component(
       ecs, sizeof(tgg_character_max_health_comp), NULL, NULL);
+}
+
+void tgg_character_name_comp_debug(tgg_character_name_comp* comp) {
+  printf("tgg_character_name_comp: %s\n", *comp);
+}
+void tgg_character_min_health_comp_debug(tgg_character_min_health_comp* comp) {
+  printf("tgg_character_min_health_comp: %f\n", *comp);
+}
+void tgg_character_max_health_comp_debug(tgg_character_max_health_comp* comp) {
+  printf("tgg_character_max_health_comp: %f\n", *comp);
 }
