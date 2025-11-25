f2ca0115c26e8902f99465c9dddffb0653abc816
KN13KOMETA <kn13kometa@gmail.com>
2025-11-25 22:51:51 +0300

    Somehow turn off pico_headers warnings

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -7,7 +7,7 @@ add_subdirectory(libs/lua)
 
 # Header-only library pico_headers
 add_library(pico_headers INTERFACE)
-target_include_directories(pico_headers INTERFACE libs/pico_headers)
+target_include_directories(pico_headers SYSTEM INTERFACE libs/pico_headers)
 
 # Link libraries
 target_link_libraries(
