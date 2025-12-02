6c46643121d951f66901a32a71d0fed8c989272a
KN13KOMETA <kn13kometa@gmail.com>
2025-12-03 01:59:58 +0300

    Remove validation system

--- src/tgg/systems.h
+++ src/tgg/systems.h
@@ -4,15 +4,12 @@
 
 #include <pico_ecs.h>
 
-typedef enum { TGG_DEBUG_FLAG = 01, TGG_VALIDATION_FLAG = 02 } tgg_flags;
+typedef enum { TGG_DEBUG_FLAG = 01 } tgg_flags;
 
 ecs_ret_t tgg_debug_system(ecs_t* ecs, ecs_entity_t* entities,
                            size_t entity_count, void* udata);
-ecs_ret_t tgg_validation_system(ecs_t* ecs, ecs_entity_t* entities,
-                                size_t entity_count, void* udata);
 
 static ecs_system_t TGG_DEBUG_SYSTEM;
-static ecs_system_t TGG_VALIDATION_SYSTEM;
 
 void tgg_register_systems(ecs_t* ecs);
 
