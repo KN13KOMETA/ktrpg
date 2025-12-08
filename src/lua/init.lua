17670db895d9a2badad02a139a798bfe054b9a2e
KN13KOMETA <kn13kometa@gmail.com>
2025-12-08 17:35:29 +0300

    Testing more lua

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -1,9 +1,22 @@
-local sid = tgg.register_character({
-  max_health = 77,
-  name = "Sexmaster69",
-})
+local component = {
+  health = lt.register_number_component(),
+  damage = lt.register_number_component(),
+  location = lt.register_integer_component(),
+}
 
-tgg.register_character({
-  name = "Goblin",
-  min_health = 14,
-})
+print(
+  string.format(
+    "COMPONENTS\n" .. "health: %d\n" .. "damage: %d\n" .. "location: %d",
+    component.health,
+    component.damage,
+    component.location
+  )
+)
+
+local system = {
+  damage_system = lt.register_system(function()
+    print("pretend like a system is working LOL")
+  end),
+}
+
+-- print(string.format("SYSTEMS\n" .. "damage_system: %d", system.damage_system))
