4b9563e2ef8421cbb3805f83044bb7fb177154fd
KN13KOMETA <kn13kometa@gmail.com>
2025-12-27 22:40:16 +0300

    Fix wrapper system

--- src/luab/luab_helper.c
+++ src/luab/luab_helper.c
@@ -1,5 +1,7 @@
 #include "luab_helper.h"
 
+#include <stdlib.h>
+
 #include "../functions.h"
 
 ecs_id_t luab_h_register_integer_component(luab_state* lb) {
@@ -15,9 +17,18 @@ ecs_id_t luab_h_register_number_component(luab_state* lb);
 ecs_id_t luab_h_register_string_component(luab_state* lb);
 
 ecs_id_t luab_h_register_system(luab_state* lb, int lua_func_ref) {
+  luab_system* lbs = malloc(sizeof(luab_system));
+  ecs_system_t s =
+      ecs_define_system(lb->ecs, 0, luab_system_wrapper, NULL, NULL, lbs);
+
+  lbs->lb = lb;
+  lbs->index = lb->system_count;
+
+  lb->systems[lb->system_count] = s;
   lb->system_lua_refs[lb->system_count] = lua_func_ref;
+  lb->system_count++;
 
   DEBUG_LOG("registered lua function by reference %d", lua_func_ref);
 
-  return lb->system_count++;
+  return s.id;
 }
