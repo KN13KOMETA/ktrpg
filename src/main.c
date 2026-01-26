11e3380938d7ce7c2b66f766cc4dff555bed120b
KN13KOMETA <kn13kometa@gmail.com>
2026-01-27 00:53:52 +0300

    User can now run custom lua files

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@
 #include "luaglue/class/component.h"
 #include "luaglue/core.h"
 #include "luah/alt.h"
+#include "luah/init.h"
+#include "luah/t.h"
 #include "project_options.h"
 
 #define PICO_ECS_IMPLEMENTATION
@@ -41,16 +43,98 @@ int main(int argc, char* argv[]) {
 
     lg_create(L, ecs);
 
-    DEBUG_LOG("AMONGUS");
+    // Load lua scripts or use internal ones
+    if (poptions->script_path != NULL &&
+        file_exists(poptions->script_path) == 0) {
+      DEBUG_LOG("RUNNING USER LUA SCRIPTS");
 
+      // TODO: Test it on windows
+      {
+        char* module_path;
+        char* path_type1 = "?.lua";
+        char* path_type2 = "?/init.lua";
+        char* target_path;
+        size_t target_path_length;
+        int i = 0;
+
+        // Init target_path
+        {
+          char* last_slash = strrchr(poptions->script_path, '/');
+          long length = last_slash - poptions->script_path + 1;
+
+          target_path = malloc(length + 1);
+
+          strncpy(target_path, poptions->script_path, length);
+
+          target_path[length] = '\0';
+        }
+
+        target_path_length = strlen(target_path);
+
+        module_path = malloc(target_path_length + strlen(path_type1) + 1 +
+                             target_path_length + strlen(path_type2) + 1);
+
+        strcpy(module_path, target_path);
+        i += target_path_length;
+
+        strcpy(module_path + i, path_type1);
+        i += strlen(path_type1);
+
+        module_path[i] = ';';
+        i++;
+
+        strcpy(module_path + i, target_path);
+        i += target_path_length;
+
+        strcpy(module_path + i, path_type2);
+
+        // Set new package.path
+        {
+          char* package_path;
+          char* current_path;
+          int i = 0;
+
+          lua_getglobal(L, "package");
+          lua_getfield(L, -1, "path");
+
+          current_path = lua_tostring(L, -1);
+
+          package_path =
+              malloc(strlen(current_path) + 1 + strlen(module_path) + 1);
+
+          strcpy(package_path, current_path);
+          i += strlen(current_path);
+
+          package_path[i] = ';';
+          i++;
+
+          strcpy(package_path + i, module_path);
+
+          lua_pop(L, 1);
+          lua_pushstring(L, package_path);
+          lua_setfield(L, -2, "path");
+          lua_pop(L, 1);
+
+          free(target_path);
+          free(module_path);
+          free(package_path);
+        }
+      }
+
+      if (luaL_dofile(L, poptions->script_path) != LUA_OK)
+        printf("Error at internal user scripts: %s\n", lua_tostring(L, -1));
+    } else {
+      DEBUG_LOG("RUNNING INTERNAL LUA SCRIPTS");
+
+      luab_register_text_module(L, "t", t_lua);
+
+      if (luaL_dostring(L, init_lua) != LUA_OK)
+        printf("Error at internal scripts: %s\n", lua_tostring(L, -1));
+    }
     if (luaL_dostring(L, alt_lua) != LUA_OK) {
       printf("EERRRRR: %s\n", lua_tostring(L, -1));
       lua_pop(L, -1);
     }
-
-    ecs_run_systems(ecs, 0);
-
-    DEBUG_LOG("AMONGUS");
 
     lg_destroy();
     ecs_free(ecs);
