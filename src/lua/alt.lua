9531913cd00479e20e256c59b9bfeceb7133198f
KN13KOMETA <kn13kometa@gmail.com>
2026-01-26 18:46:48 +0300

    New test file

--- src/lua/alt.lua
+++ src/lua/alt.lua
@@ -11,13 +11,40 @@ COMPONENT = {
   damage_received = ktrpg.Component:new("num", "Damage Received"),
 }
 
-SYSTEMS = {
-  print_name = ktrpg.System
+SYSTEM = {
+  print_name = ktrpg
+    .System
     :new("Print Name")
     :requires(COMPONENT.name)
-    :excludes(COMPONENT.attack_target)
-    :set_mask(math.maxinteger),
+    -- :excludes(COMPONENT.attack_target)
+    :set_mask(math.maxinteger)
+    :on_run(function(entities, entity_count)
+      print("Yo this is ma print name function from lua")
+      io.write("we have that much en titties: ")
+      print(entity_count)
+
+      for i = 1, entity_count, 1 do
+        print("entity " .. i .. " name is " .. entities[i]:get(COMPONENT.name))
+      end
+    end),
 }
 
-print(SYSTEMS.print_name:get_name())
-print(SYSTEMS.print_name:get_mask())
+local player = ktrpg.Entity:new()
+print(player:set(COMPONENT.name, "POPPO"))
+print(player:set(COMPONENT.is_player, 20))
+print(player:set(COMPONENT.max_health, 1.3))
+print(player:set(COMPONENT.attack_target, 69))
+
+print(SYSTEM.print_name:get_name())
+
+print(player:get(COMPONENT.name, "POPPO"))
+print(player:get(COMPONENT.is_player, 20))
+print(player:get(COMPONENT.max_health, 1.3))
+print(player:get(COMPONENT.attack_target, 69))
+print(player:get(COMPONENT.attack, 69))
+
+SYSTEM.print_name:run():run():run():run()
+
+print(SYSTEM.print_name:get_mask())
+
+print(SYSTEM.print_name:get_entity_count())
