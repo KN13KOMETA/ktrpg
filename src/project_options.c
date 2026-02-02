1ea5a4f0023cc5edb30a141afcc7713d81dbaa1e
KN13KOMETA <kn13kometa@gmail.com>
2026-02-02 16:35:58 +0300

    Rename struct field

--- src/project_options.c
+++ src/project_options.c
@@ -26,7 +26,7 @@ project_options* prompt_project_options(int argc, const char* argv[]) {
   project_options* poptions;
   SELFMALLOC(poptions);
 
-  poptions->export_path = NULL;
+  poptions->scripts_export_path = NULL;
   poptions->script_path = NULL;
 
   // NOTE: These two unused, but required for value in callback
@@ -40,7 +40,7 @@ project_options* prompt_project_options(int argc, const char* argv[]) {
                   version_opt_cb, 0, OPT_NONEG),
       OPT_GROUP("Lua scripts options"),
       OPT_STRING('x', "export", &export_value,
-                 "export internal scripts to specified path", export_opt_cb,
+                 "export internal lua scripts to specified path", export_opt_cb,
                  (intptr_t)poptions, OPT_NONEG),
       OPT_STRING('u', "user", &user_value,
                  "loads game content using script by specified path",
@@ -62,8 +62,8 @@ void print_version(void) {
   exit(EXIT_SUCCESS);
 }
 void export_scripts(char* path, project_options* poptions) {
-  FMALLOC(poptions->export_path, strlen(path) + 1);
-  strcpy(poptions->export_path, path);
+  FMALLOC(poptions->scripts_export_path, strlen(path) + 1);
+  strcpy(poptions->scripts_export_path, path);
 }
 void use_user_script(char* path, project_options* poptions) {
   FMALLOC(poptions->script_path, strlen(path) + 1);
@@ -104,7 +104,7 @@ int user_opt_cb(struct argparse* self, const struct argparse_option* option) {
 
 void free_project_options(project_options* options) {
   if (options->script_path != NULL) free(options->script_path);
-  if (options->export_path != NULL) free(options->export_path);
+  if (options->scripts_export_path != NULL) free(options->scripts_export_path);
 
   free(options);
 }
