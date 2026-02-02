088e298c3e40424ffe2931dc26e240084a10e0d3
KN13KOMETA <kn13kometa@gmail.com>
2026-02-02 14:56:48 +0300

    Add System types

--- src/lua/ktrpg.d.lua
+++ src/lua/ktrpg.d.lua
@@ -103,10 +103,63 @@ function Entity:set_limit(limit) end
 ---@return integer limit
 function Entity:get_limit() end
 
+---@class SystemInstance
+local SystemInstance = {}
+
+---@return string name
+function SystemInstance:get_name() end
+
+---@param ... ComponentInstance
+---@return SystemInstance
+function SystemInstance:requires(...) end
+
+---@param ... ComponentInstance
+---@return SystemInstance
+function SystemInstance:excludes(...) end
+
+---@param mask integer
+---@return SystemInstance
+function SystemInstance:set_mask(mask) end
+
+---@return integer mask
+function SystemInstance:get_mask() end
+
+---@return SystemInstance
+function SystemInstance:enable() end
+
+---@return SystemInstance
+function SystemInstance:disable() end
+
+---@return SystemInstance
+function SystemInstance:on_run() end
+
+---@return SystemInstance
+function SystemInstance:run() end
+
+---@return integer entities_count
+function SystemInstance:get_entity_count() end
+
+---@class System
+local System = {}
+
+---@param name string
+---@return SystemInstance
+---@overload fun(self: System, name: string): nil, string
+function System:new(name) end
+
+---@param limit integer
+---@return integer limit
+---@overload fun(self: System, limit: integer): nil, string
+function System:set_limit(limit) end
+
+---@return integer limit
+function System:get_limit() end
+
 ---@class ktrpg
 local ktrpg = {
   Entity = Entity,
   Component = Component,
+  System = System,
 }
 
 return ktrpg
