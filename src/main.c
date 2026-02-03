5c555043cd92e05bc5effbdd70634958c96ff7b6
KN13KOMETA <kn13kometa@gmail.com>
2026-02-03 23:32:55 +0300

    Replace game loading with 2 functions

--- src/main.c
+++ src/main.c
@@ -24,6 +24,8 @@
 
 void print_debug_mode(void);
 int user_script_warning(void);
+int init_game(char* script_path);
+int init_embedded_game(vfile* scripts);
 
 int main(int argc, char* argv[]) {
   vfile vfscripts[] = {
@@ -37,6 +39,7 @@ int main(int argc, char* argv[]) {
   launch_options loptions = {0, 0, NULL, NULL, NULL};
   int code = process_launch_options(&loptions, argc, argv);
 
+  // Handle process_launch_options exit code
   switch (code) {
     case LO_EXIT_ACTION:
       return EXIT_SUCCESS;
@@ -54,72 +57,24 @@ int main(int argc, char* argv[]) {
   printf(TITLE("HELP"));
   printf("Run with --help for more info\n");
 
-  if (loptions.scripts_export_path != NULL)
+  if (loptions.scripts_export_path != NULL) {
     if (write_vfiles_to_dir(vfscripts, loptions.scripts_export_path)) {
       printf("Exporting scripts failed\n");
       return EXIT_FAILURE;
     }
+  }
 
-  if (loptions.types_export_path != NULL)
+  if (loptions.types_export_path != NULL) {
     if (write_vfiles_to_dir(vftypes, loptions.types_export_path)) {
       printf("Exporting types failed\n");
       return EXIT_FAILURE;
     }
-
-  {
-    lua_State* L = luaL_newstate();
-    ecs_t* ecs = ecs_new(128, NULL);
-
-    luaL_openlibs(L);
-
-    lg_create(L, ecs);
-
-    // Load lua scripts or use internal ones
-    if (loptions.script_path != NULL) {
-      char* target_path;
-      char* module_path;
-
-      if (user_script_warning()) return EXIT_SUCCESS;
-
-      printf(TITLE("GAME (user scripts)"));
-
-      // TODO: Test it on windows
-
-      // Init target_path
-      {
-        char* last_slash = strrchr(loptions.script_path, '/');
-        unsigned long length =
-            (unsigned long)(last_slash - loptions.script_path);
-
-        FMALLOC(target_path, length + 1);
-
-        strncpy(target_path, loptions.script_path, length);
-
-        target_path[length] = '\0';
-      }
-
-      module_path = build_lua_package_search_path(target_path);
-
-      add_lua_package_path(L, module_path);
-
-      free(target_path);
-      free(module_path);
-
-      if (luaL_dofile(L, loptions.script_path) != LUA_OK)
-        printf("Error at internal user scripts: %s\n", lua_tostring(L, -1));
-    } else {
-      printf(TITLE("GAME (internal scripts)"));
-
-      register_lua_text_module(L, "module", module_lua);
-
-      if (luaL_dostring(L, init_lua) != LUA_OK)
-        printf("Error at internal scripts: %s\n", lua_tostring(L, -1));
-    }
-
-    lg_destroy();
-    ecs_free(ecs);
-    lua_close(L);
   }
+
+  if (loptions.script_path != NULL)
+    return init_game(loptions.script_path);
+  else
+    return init_embedded_game(vfscripts);
 
   return EXIT_SUCCESS;
 }
@@ -151,4 +106,89 @@ int user_script_warning(void) {
     default:
       return 1;
   }
+}
+
+// TODO: Check size settings on ecs_new
+
+int init_game(char* script_path) {
+  int code = EXIT_SUCCESS;
+  char basedir[strlen(script_path)];
+  char* module_path;
+  lua_State* L = luaL_newstate();
+  ecs_t* ecs = ecs_new(128, NULL);
+
+  luaL_openlibs(L);
+  lg_create(L, ecs);
+
+  if (user_script_warning()) return EXIT_SUCCESS;
+
+  printf(TITLE("GAME (user scripts)"));
+
+  // We only need to set up package.path
+  // if file not in the same workdir
+  if (get_basedir(script_path, basedir) == 0) {
+    module_path = build_lua_package_search_path(basedir);
+    add_lua_package_path(L, module_path);
+  }
+
+  if (luaL_dofile(L, script_path) != LUA_OK) {
+    printf("Lua error: %s\n", lua_tostring(L, -1));
+    code = EXIT_FAILURE;
+  }
+
+  lg_destroy();
+  ecs_free(ecs);
+  lua_close(L);
+
+  return code;
+}
+
+int init_embedded_game(vfile* scripts) {
+  int code = EXIT_SUCCESS;
+  int script_count = 0;
+  lua_State* L;
+  ecs_t* ecs;
+
+  for (int i = 0; scripts[i].path != NULL; i++) script_count++;
+
+  printf(TITLE("GAME (embedded scripts)"));
+
+  if (script_count == 0) {
+    printf("Fail to load embedded game, expected at least 1 scripts\n");
+    return EXIT_FAILURE;
+  }
+
+  L = luaL_newstate();
+  ecs = ecs_new(128, NULL);
+
+  luaL_openlibs(L);
+  lg_create(L, ecs);
+
+  for (int i = 1; i < script_count; i++) {
+    vfile f = scripts[i];
+    size_t path_len = strlen(f.path);
+    char* ext = strrchr(f.path, '.');
+    char module_name[path_len - 4 + 1];
+
+    if (ext == NULL || strcmp(".lua", ext) != 0) {
+      printf("Fail to load embedded game, unknown file \"%s\"\n", f.path);
+      code = EXIT_FAILURE;
+    }
+
+    strncpy(module_name, f.path, path_len - 4);
+    module_name[path_len - 4] = '\0';
+
+    register_lua_text_module(L, module_name, f.content);
+  }
+
+  if (code == EXIT_SUCCESS && luaL_dostring(L, scripts[0].content) != LUA_OK) {
+    printf("Lua error: %s\n", lua_tostring(L, -1));
+    code = EXIT_FAILURE;
+  }
+
+  lg_destroy();
+  ecs_free(ecs);
+  lua_close(L);
+
+  return code;
 }
