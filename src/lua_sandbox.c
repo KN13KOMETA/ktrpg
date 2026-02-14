dcf1b40772563cc421c2cce53923c429c006b441
KN13KOMETA <kn13kometa@gmail.com>
2026-02-14 23:40:40 +0300

    Add lua sandbox

---
+++ src/lua_sandbox.c
@@ -0,0 +1,115 @@
+#include "lua_sandbox.h"
+
+#include <lauxlib.h>
+#include <lua.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "functions.h"
+
+#define LSB_REG "krequire_loaded"
+
+static char* wd;
+static vfile* vlibs;
+
+static int ll_krequire(lua_State* L) {
+  const char* name = luaL_checkstring(L, 1);
+
+  lua_getfield(L, LUA_REGISTRYINDEX, LSB_REG);
+
+  // Returning cache if there's any
+  lua_getfield(L, -1, name);
+  if (!lua_isnil(L, -1)) {
+    lua_remove(L, -2);
+    return 1;
+  }
+  lua_pop(L, 1);
+
+  {
+    const char* vcontent = NULL;
+    int status;
+
+    // Searching module in vlibs first
+    if (vlibs != NULL) {
+      vfile* f = vlibs;
+      char fname[strlen(name) + 4 + 1];
+
+      sprintf(fname, "%s.lua", name);
+
+      while (f->path != NULL) {
+        if (strcmp(fname, f->path) == 0) {
+          vcontent = f->content;
+          break;
+        }
+
+        f++;
+      }
+    }
+
+    if (vcontent != NULL) {
+      status = luaL_loadbuffer(L, vcontent, strlen(vcontent), name);
+    } else if (wd != NULL) {
+      // Searching module in wd
+      char fname[strlen(wd) + 1 + strlen(name) + 4 + 1];
+
+      sprintf(fname, "%s/%s.lua", wd, name);
+
+      status = luaL_loadfile(L, fname);
+    } else {
+      lua_pushfstring(L, "module '%s' not found", name);
+      return lua_error(L);
+    }
+
+    if (status != LUA_OK) return lua_error(L);
+
+    // Run module
+    if (lua_pcall(L, 0, LUA_MULTRET, 0) != LUA_OK) return lua_error(L);
+
+    // Handle return value
+    {
+      int nret = lua_gettop(L) - 2;
+      if (nret == 0) {
+        lua_pushboolean(L, 1);
+        nret = 1;
+      }
+
+      if (nret > 1) {
+        lua_pushvalue(L, 2);
+        lua_replace(L, 2);
+        lua_settop(L, 2);
+      }
+    }
+  }
+
+  // Save result in cache
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -3, name);
+  lua_remove(L, -2);
+
+  return 1;
+}
+
+void lsb_create(lua_State* L, vfile* virtual_libs, char* workdir_path) {
+  DEBUG_LOG("LSB: CREATE");
+
+  vlibs = virtual_libs;
+
+  if (workdir_path != NULL) {
+    FMALLOC(wd, strlen(workdir_path) + 1);
+    strcpy(wd, workdir_path);
+  } else {
+    wd = NULL;
+  }
+
+  lua_newtable(L);
+  lua_pushvalue(L, -1);
+  lua_setfield(L, LUA_REGISTRYINDEX, LSB_REG);
+  lua_pop(L, 1);
+
+  lua_register(L, "krequire", ll_krequire);
+}
+void lsb_destroy(void) {
+  DEBUG_LOG("LSB: DESTROY");
+
+  if (wd != NULL) free(wd);
+}
---
+++ src/lua_sandbox.h
@@ -0,0 +1,15 @@
+#ifndef LUA_SANDBOX_H
+#define LUA_SANDBOX_H
+
+#include <lua.h>
+
+#include "constants.h"
+
+// Creates lua sandbox
+// workdir_path == NULL then only virtual_libs will be used
+// virtual_libs == NULL then only workdir_path will be used
+// If both NULL, well no libs will be found
+void lsb_create(lua_State* L, vfile* virtual_libs, char* workdir_path);
+void lsb_destroy(void);
+
+#endif  // !LUA_SANDBOX_H
