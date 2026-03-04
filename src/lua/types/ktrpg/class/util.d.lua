04776522b5acfd5fcb2d848b46187c088e9ce6ec
KN13KOMETA <kn13kometa@gmail.com>
2026-03-04 19:25:41 +0300

    Add util and krequire types

---
+++ src/lua/types/ktrpg/class/util.d.lua
@@ -0,0 +1,31 @@
+---@meta
+
+---@class Util
+local Util = {}
+
+-- read", util_read},
+-- readchar", util_readchar},
+-- ask_yn", util_ask_yn},
+-- writenl", util_writenl},
+-- write", util_write},
+-- sleep", util_sleep},
+
+---@param length integer
+---@return string
+function Util.read(length) end
+
+---@return string
+function Util.readchar() end
+
+---@param question integer
+---@return boolean
+function Util.ask_yn(question) end
+
+---@param text string
+function Util.writenl(text) end
+
+---@param text string
+function Util.write(text) end
+
+---@param seconds integer
+function Util.sleep(seconds) end
