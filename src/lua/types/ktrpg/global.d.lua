0d8dc2ebc14ad747a0210132a1bfcfbedabd7f0a
KN13KOMETA <kn13kometa@gmail.com>
2026-03-08 18:23:43 +0300

    Fix types

--- src/lua/types/ktrpg/global.d.lua
+++ src/lua/types/ktrpg/global.d.lua
@@ -1,6 +1,3 @@
----@class _G
----@field krequire integer
-
 ---@overload fun(modname: "ktrpg"): ktrpg
 ---@overload fun(modname: string): unknown
 krequire = function() end ---@diagnostic disable-line: lowercase-global
