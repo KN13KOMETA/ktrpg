00543fce51a6d9f1adc3fb4bb00b02f6ce3b64e6
KN13KOMETA <kn13kometa@gmail.com>
2026-03-23 15:51:02 +0300

    Move filter in other file

--- zgame/world/extensions/ktrpgmapformat/index.mjs
+++ zgame/world/extensions/ktrpgmapformat/index.mjs
@@ -1,4 +1,5 @@
-import { parseLuaTemplate } from "./ktrpgmapformat/function/parseLuaTemplate.mjs";
+import { filterObjects } from "./function/filterObjects.mjs";
+import { parseLuaTemplate } from "./function/parseLuaTemplate.mjs";
 
 const getPos = ({ x, y }) => `x${Math.floor(x)}y${Math.floor(y)}`;
 
@@ -14,34 +15,7 @@ tiled.registerMapFormat("ktrpgmap", {
   extension: "lua",
 
   write: (map, fileName) => {
-    const locationObjects = [];
-    const doorObjects = [];
-
-    for (let li = 0; li < map.layerCount; li++) {
-      /**@type ObjectGroup */
-      const layer = map.layers[li];
-
-      if (!layer.isObjectLayer) return;
-
-      for (let oi = 0; oi < layer.objectCount; oi++) {
-        const object = layer.objects[oi];
-
-        switch (object.className) {
-          case "Location": {
-            locationObjects.push(layer.objects[oi]);
-            break;
-          }
-          case "Door": {
-            doorObjects.push(layer.objects[oi]);
-            break;
-          }
-          default:
-            tiled.error(
-              `Unknown Class: object (${object.id}) at layer ${layer.name}`,
-            );
-        }
-      }
-    }
+    const fobj = filterObjects(map);
 
     const lmap = new Map();
 
@@ -50,11 +24,11 @@ tiled.registerMapFormat("ktrpgmap", {
       doors: [],
     };
 
-    for (let li = 0; li < locationObjects.length; li++) {
-      const lobj = locationObjects[li];
+    for (let li = 0; li < fobj.locations.length; li++) {
+      const location = fobj.locations[li];
       const lnew = {
-        value: lobj.property("value")?.value,
-        name: `"${lobj.property("name")}"`,
+        value: location.property("value")?.value,
+        name: `"${location.property("name")}"`,
       };
       let skip = false;
 
@@ -71,7 +45,7 @@ tiled.registerMapFormat("ktrpgmap", {
 
       insert.locations.push(lnew);
 
-      lmap.set(getPos(lobj), lnew);
+      lmap.set(getPos(location), lnew);
     }
 
     tiled.log(lmap.size);
@@ -79,12 +53,12 @@ tiled.registerMapFormat("ktrpgmap", {
       tiled.log(k);
     });
 
-    for (let di = 0; di < doorObjects.length; di++) {
-      const dobj = doorObjects[di];
+    for (let di = 0; di < fobj.doors.length; di++) {
+      const door = fobj.doors[di];
       const dnew = {
-        name: dobj.property("name").charCodeAt(0),
-        location: dobj.property("location")?.value,
-        destination: dobj.property("destination")?.value,
+        name: door.property("name").charCodeAt(0),
+        location: door.property("location")?.value,
+        destination: door.property("destination")?.value,
       };
       let skip = false;
 
@@ -93,7 +67,7 @@ tiled.registerMapFormat("ktrpgmap", {
         skip = true;
       }
       if (dnew.location == null) {
-        const x = lmap.get(getPos(dobj)).value;
+        const x = lmap.get(getPos(door)).value;
         if (x != null) {
           dnew.location = x;
         } else {
