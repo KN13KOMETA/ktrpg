5aaee1e8c133b4eb3a7522fadea2c5f364909d14
KN13KOMETA <kn13kometa@gmail.com>
2026-03-26 18:57:58 +0300

    Get rid of old components

--- zgame/init.lua
+++ zgame/init.lua
@@ -4,9 +4,6 @@ local ktrpg = krequire("ktrpg")
 
 local init_world = krequire("game/world")
 
----@type GameComponents
-local GComponents = krequire("game/components")
-
 ---@type GComponent
 local GComponent = krequire("class/GComponent")
 local gcomp = GComponent:new()
@@ -17,16 +14,6 @@ local System = ktrpg.System
 local Util = ktrpg.Util
 
 -- TODO: empty numbers to nvoid
-
-local comp = GComponents.create(Component)
-
-local syst_cache = {
-  move = {
-    location = {},
-    clocation = -1,
-    doors = {},
-  },
-}
 
 local sc = {
   locations = {},
@@ -141,67 +128,45 @@ local syst = {
       sc.group.items_by_creature = t
     end
   end),
-  move_get_location_names = System:new("MOVE: Get Location Names")
-    :requires(comp.location, comp.name)
+  move_creatures_move = System:new("MOVE: Creatures Move")
+    :requires(gcomp.creature, gcomp.location)
     :on_run(function(entities, entities_count)
       for i = 1, entities_count, 1 do
         local e = entities[i]
-        syst_cache.move.location[e:get(comp.location)] = e:get(comp.name)
-      end
-    end),
-  move_get_current_location = System:new("MOVE: Get Current Location")
-    :requires(comp.player, comp.location)
-    :on_run(function(entities, entities_count)
-      if entities_count == 0 then
-        syst_cache.move.clocation = -1
-        return
-      end
+        local creature = sc.creatures[e:get(gcomp.creature)]
 
-      syst_cache.move.clocation = entities[1]:get(comp.location)
-    end),
-  move_find_doors = System:new("MOVE: Find Doors")
-    :requires(comp.door, comp.location, comp.location_next)
-    :on_run(function(entities, entities_count)
-      local doors = {}
+        Util.writenl(creature)
 
-      if syst_cache.move.clocation ~= -1 then
-        for i = 1, entities_count, 1 do
-          local e = entities[i]
-          local cl = e:get(comp.location)
-          if cl == syst_cache.move.clocation then
-            doors[e:get(comp.door)] = e:get(comp.location_next)
+        -- TODO: implement other creatures moving
+        if creature.id == gcomp.creature_player then
+          Util.writenl("Current location \"" .. sc.locations[creature.location].name .. "\"")
+
+          local sorted_keys = {}
+
+          for key in pairs(sc.group.doors_by_location[creature.location]) do
+            table.insert(sorted_keys, key)
           end
+
+          table.sort(sorted_keys)
+
+          for _, key in ipairs(sorted_keys) do
+            local door = sc.group.doors_by_location[creature.location][key]
+            Util.writenl(":> Door '" .. string.char(door.name) .. "': " .. sc.locations[door.destination].name)
+          end
+
+          Util.write("Select door: ")
+
+          local sel = string.byte(Util.readchar())
+          local sel_door = sc.group.doors_by_location[creature.location][sel]
+
+          if sel_door ~= nil then
+            e:set(gcomp.location, sel_door.destination)
+          end
+
         end
-      end
-
-      syst_cache.move.doors = doors
-    end),
-  move_player_move = System:new("MOVE: Player Move")
-    :requires(comp.player, comp.location, comp.movable)
-    :on_run(function(entities, entities_count)
-      if entities_count == 0 then
-        return
-      end
-
-      Util.writenl("Current location \"" .. syst_cache.move.location[syst_cache.move.clocation] .. "\"")
-
-      for door, next in pairs(syst_cache.move.doors) do
-        Util.writenl(":> Door '" .. string.char(door) .. "': " .. syst_cache.move.location[next])
-      end
-
-      Util.write("Select door: ")
-
-      local sel = string.byte(Util.readchar())
-      local sel_door = syst_cache.move.doors[sel]
-
-      if sel_door ~= nil then
-        Util.writenl("select " .. sel_door)
-        entities[1]:set(comp.location, sel_door)
-        entities[1]:set(gcomp.location, sel_door)
       end
     end),
   debug_cache = System:new("Debug Cache"):on_run(function(entities, entities_count)
-    Util.writenl(syst_cache)
     Util.writenl(sc)
     Util.writenl(sc.group)
   end),
@@ -216,10 +181,7 @@ local function run_cache()
 end
 
 local function run_move()
-  syst.move_get_location_names:run()
-  syst.move_get_current_location:run()
-  syst.move_find_doors:run()
-  syst.move_player_move:run()
+  syst.move_creatures_move:run()
 end
 
 local function run()
@@ -231,19 +193,6 @@ local function run()
   syst.debug:run()
 end
 
--- for i = 1, #location_doors, 1 do
---   local door = location_doors[i]
---   local e = Entity:new()
---
---   if e == nil then
---     return Util.exit()
---   end
---
---   e:set(comp.door, string.byte(door[1]))
---   e:set(comp.location, door[2])
---   e:set(comp.location_next, door[3])
--- end
-
 init_world(function(l_tag, l_name)
   local e = Entity:new()
 
@@ -253,9 +202,6 @@ init_world(function(l_tag, l_name)
 
   e:set(gcomp.location, l_tag)
   e:set(gcomp.name, l_name)
-
-  e:set(comp.location, l_tag)
-  e:set(comp.name, l_name)
 end, function(d_tag, d_location, d_destination)
   local e = Entity:new()
 
@@ -266,11 +212,6 @@ end, function(d_tag, d_location, d_destination)
   e:set(gcomp.door, d_tag)
   e:set(gcomp.location, d_location)
   e:set(gcomp.destination, d_destination)
-
-  e:set(comp.door, d_tag)
-  e:set(comp.location, d_location)
-  e:set(comp.location_next, d_destination)
-
 end)
 
 do
@@ -279,10 +220,6 @@ do
   if e == nil then
     return Util.exit()
   end
-
-  e:set(comp.player, 0)
-  e:set(comp.location, 0)
-  e:set(comp.movable, 0)
 
   e:set(gcomp.creature, gcomp.creature_player)
   e:set(gcomp.location, 0)
