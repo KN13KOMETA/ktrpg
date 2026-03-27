cea124ae5493c34950369ce92e417fadec1af437
KN13KOMETA <kn13kometa@gmail.com>
2026-03-27 21:59:08 +0300

    Add SplitMix64

--- src/functions.c
+++ src/functions.c
@@ -31,6 +31,17 @@ void sleep_s(unsigned seconds) {
 #endif  // ifdef _WIN32
 }
 
+// SplitMix64
+// Written by Sebastiano Vigna (vigna@acm.org)
+// For license check:
+// https://prng.di.unimi.it/splitmix64.c
+uint64_t rnd64_next(uint64_t* state) {
+  uint64_t z = (*state += 0x9e3779b97f4a7c15);
+  z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+  z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+  return z ^ (z >> 31);
+}
+
 int getchar_clear(char* ch) {
   int c = 0;
 
